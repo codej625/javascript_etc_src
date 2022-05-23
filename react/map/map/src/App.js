a43764890466169f4cde15174fb8b8fb625675ed
codej625 <codej625@gmail.com>
2022-05-24 00:03:52 +0900

    20220524_react_map_readme_update

---
+++ react/map/map/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css';
+import Array from './component/Array';
+//=========================================================================
+const App = () => {
+  const [tableTHData, setTableTHData] = useState([
+    { label: 'seq', order: 'asc', show: false },
+    { label: 'time', order: 'asc', show: true },
+    { label: 'ip', order: 'asc', show: false },
+    { label: 'device', order: 'asc', show: true },
+    { label: 'hostname', order: 'asc', show: true },
+    { label: 'pathname', order: 'asc', show: true },
+    { label: 'sourcecode', order: 'asc', show: true },
+    { label: 'mediumcode', order: 'asc', show: true },
+    { label: 'campaigncode', order: 'asc', show: true },
+    { label: 'termcode', order: 'asc', show: true },
+    { label: 'contentscode', order: 'asc', show: true },
+    { label: 'userdata', order: 'asc', show: true },
+    { label: 'extradata', order: 'asc', show: false }
+  ]);
+  return (
+//=========================================================================
+    <React.Fragment>
+      <div>{tableTHData[0].show}</div>
+    </React.Fragment>
+//=========================================================================
+  );
+}
+//=========================================================================
+export default App;
