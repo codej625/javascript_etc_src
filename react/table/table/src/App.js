5b90e715e1787acb3ef7b42aa84fe13fc09df238
codej625 <codej625@gmail.com>
2022-06-18 22:15:33 +0900

    20220618_update

--- react/table/table/src/App.js
+++ react/table/table/src/App.js
@@ -1,42 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import datas from './data.json';
-
+//=======================================================
 const App = () => {
+
   const [tableHead, setTableHead] = useState([]);
   const [editIdx, setEditIdx] = useState(9999);
   const [chageData, setChangeData] = useState("");
-  
+  //=====================================================
   useEffect(() => {
     console.log('한번');
   }, []);
-
+  
   useEffect(() => {
     console.log('여러번');
   });
-
+  
   useEffect(() => {
     console.log(`changeData 변경 : ${chageData}`);
-  }, [chageData]);  
-
+  }, [chageData]);
+  
   const getTargetData = (data) => {
     console.log('data', data);
   }
-
+  
   const editData = (idx) => {
     console.log(idx);
     setEditIdx(idx);
   }
-
+  //=====================================================
   return (
     <table>
       <thead>
         <tr>
-        {Object.keys(datas[0]).map((key,idx) => (
-          <td key={idx}>{key}</td>          
-        ))}
-        <td>수정하기</td>
+          {Object.keys(datas[0]).map((key, idx) => (
+            <td key={idx}>{key}</td>
+          ))}
+          <td>수정하기</td>
         </tr>
       </thead>
       <tbody>
@@ -64,12 +64,12 @@ const App = () => {
         ))*/}
         {datas.map((data, idx) => {
           let background = "";
-          switch(data.Score){
+          switch (data.Score) {
             case 50:
               background = 'red';
               break;
             case 0:
-              background =  'white';
+              background = 'white';
               break;
             default:
               background = 'blue';
@@ -77,9 +77,9 @@ const App = () => {
           return (
             <tr>
               <td>
-                {(idx===editIdx)?(
+                {(idx === editIdx) ? (
                   <input type="text" value={chageData} onChange={(e) => setChangeData(e.target.value)} />
-                ):(
+                ) : (
                   <span>
                     {data.일자}
                   </span>
@@ -100,14 +100,15 @@ const App = () => {
               <td>{data.전환율}</td>
               <td>{data.CPA}</td>
               <td>{data.Score}</td>
-              <td style={{background, minWidth: 30}}>{data.Status}</td>
+              <td style={{ background, minWidth: 30 }}>{data.Status}</td>
               <td><button onClick={() => getTargetData(data.광고소재)}>자세히보기</button></td>
               <td><button onClick={() => editData(idx)}>수정하기</button></td>
             </tr>
           )
-        })}        
+        })}
       </tbody>
     </table>
   )
 }
+//=======================================================
 export default App;
