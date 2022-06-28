0f70d260d769c5f5cb4985946fae5dce9aaf42cb
codej625 <codej625@gmail.com>
2022-06-28 12:23:45 +0900

    20220628_create

--- react/table_crud/table/src/App.js
+++ react/table_crud/table/src/App.js
@@ -11,53 +11,50 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import { Checkbox } from '@material-ui/core';
 
 const App = () => {
   const [select, setSelect] = useState(false);
-  const [addText, setAddText] = useState('ADD');
   const [todos, setTodos] = useState(false);
+  const [addText, setAddText] = useState('ADD');
   const [spacing, setSpacing] = React.useState(2);
   const [inputs, setInputs] = useState({
-    'userId': '',
-    'id': '',
-    'title': '',
-    'completed': false
+    userId: '',
+    id: '',
+    title: '',
+    completed: ''
   });
-
-  const { id, title, completed } = inputs;
-
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
 
   useEffect(() => {
     setTodos(datas)
   }, []);
 
-  // useEffect(() => {
-  //   getTodos();
-  // }, []);
+  const { userId, id, title, completed } = inputs;
 
-  // useEffect(() => {
-  //   console.log(todos);
-  // },[todos]);
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs, [name]: value
+    });
+  }
 
-  // const getTodos = () => {
-  //   axios.get('https://jsonplaceholder.typicode.com/todos')
-  //     .then(res => setTodos(res.data))
-  //     .catch(err => console.log(err));
-  // }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos, inputs
+    ]);
+    setSelect(!select);
+    setAddText('ADD')
+    setInputs({
+      userId: '',
+      id: '',
+      title: '',
+      completed: ''
+    })
+  };
 
   const deleteButton = (test) => {
     if (window.confirm('삭제하시겠습니까?')) {
-      alert('어림도없지')
-      console.log(test)
       setTodos(todos.filter(item => item.id !== test));
     };
   };
@@ -70,70 +67,72 @@ const App = () => {
     } else {
       setAddText('CANCEL')
     }
-    console.log('test')
   };
-
 
   return (
     <Grid container spacing={spacing}>
       <Grid item xs={12}>
         {todos && (
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                {
-                  Object.keys(todos[0]).map((data, idx) => {
-                    // const colSpan = data === 'completed' ? 2 : 1;
-                    const align = (data === 'completed') ? 'center' : 'left';
-                    return (
-                      <TableCell key={idx} align={align}>{data}</TableCell>
-                    )
-                  })
-                }
-                <TableCell colSpan={2} align="right"><Button size="small" variant="contained" onClick={addButton}>{addText}</Button></TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {select && (
+          <form onSubmit={handleSubmit}>
+            <Table aria-label="simple table">
+              <TableHead>
                 <TableRow>
-                  <Item
-                    id={id}
-                    title={title}
-                    completed={completed}
-                    onChange={onChange}
-                  />
+                  {
+                    Object.keys(todos[0]).map((data, idx) => {
+                      // const colSpan = data === 'completed' ? 2 : 1;
+                      const align = (data === 'completed') ? 'center' : 'left';
+                      return (
+                        <TableCell key={idx} align={align}>{data}</TableCell>
+                      )
+                    })
+                  }
+                  <TableCell colSpan={2} align="right"><Button size="small" variant="contained" onClick={addButton}>{addText}</Button></TableCell>
                 </TableRow>
-              )}
-              {
-                todos.map(todo => (
-                  <TableRow key={todo.id}>
-                    <TableCell>{todo.id}</TableCell>
-                    <TableCell>{todo.userId}</TableCell>
-                    <TableCell>{todo.title}</TableCell>
-                    <TableCell align="center" ><Checkbox onChange={() => console.log(123)} /></TableCell>
-                    <TableCell colSpan={2} align="right">
-                      <Button size="small" variant="contained">EDIT</Button>
-                      <Button size="small" variant="contained" color="secondary" onClick={() => deleteButton(todo.id)}>DELETE</Button>
-                    </TableCell>
+              </TableHead>
+              <TableBody>
+                {select && (
+                  <TableRow>
+                    <Item
+                      id={id}
+                      userId={userId}
+                      title={title}
+                      completed={completed}
+                      onChange={onChange}
+                      onSubmit={handleSubmit}
+                    />
                   </TableRow>
-                ))
-              }
-            </TableBody>
-          </Table>
+                )}
+                {
+                  todos.map(todo => (
+                    <TableRow key={todo.id}>
+                      <TableCell>{todo.id}</TableCell>
+                      <TableCell>{todo.userId}</TableCell>
+                      <TableCell>{todo.title}</TableCell>
+                      <TableCell align="center" ><Checkbox /></TableCell>
+                      <TableCell colSpan={2} align="right">
+                        <Button size="small" variant="contained">EDIT</Button>
+                        <Button size="small" variant="contained" color="secondary" onClick={() => deleteButton(todo.id)}>DELETE</Button>
+                      </TableCell>
+                    </TableRow>
+                  ))
+                }
+              </TableBody>
+            </Table>
+          </form>
         )}
       </Grid>
     </Grid>
   );
 }
 
-const Item = () => {
+const Item = ({ userId, id, title, completed, onChange }) => {
   return (
     <Fragment>
-      <TableCell></TableCell>
-      <TableCell><input name="id" /></TableCell>
-      <TableCell><input name="title" /></TableCell>
-      <TableCell align="center"><Checkbox /></TableCell>
-      <TableCell align="right"><Button size="small" variant="contained" color="primary">CREATE</Button></TableCell>
+      <TableCell><input type="text" name="userId" value={userId} onChange={onChange} /></TableCell>
+      <TableCell><input type="text" name="id" value={id} onChange={onChange} /></TableCell>
+      <TableCell><input type="text" name="title" value={title} onChange={onChange} /></TableCell>
+      <TableCell align="center"><Checkbox name="completed" value={completed} onChange={onChange} /></TableCell>
+      <TableCell align="right"><Button size="small" variant="contained" color="primary" type="submit">CREATE</Button></TableCell>
     </Fragment>
   )
 }
