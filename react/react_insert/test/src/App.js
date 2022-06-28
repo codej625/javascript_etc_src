1f39d6d5271d84d5f2f579f91ff9467192426491
codej625 <codej625@gmail.com>
2022-06-28 12:37:24 +0900

    20220628_react_insert

---
+++ react/react_insert/test/src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import datas from './todos.json';
+import { Fragment, useEffect, useState } from 'react';
+function App() {
+  const [todos, setTodos] = useState(false);
+
+  useEffect(() => {
+    setTodos(datas);
+  },[]);
+
+  useEffect(() => {
+    console.log(todos)
+  },[todos]);
+
+  useEffect(() => {
+    console.log('userId-> ' + userId)
+    console.log('id-> ' + id)
+    console.log('title-> ' + title)
+    console.log('completed-> ' + completed)
+  });
+
+  const [inputs, setInputs] = useState({
+    userId: '',
+    id: '',
+    title: '',
+    completed: ''
+  });
+
+  const { userId, id, title, completed } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+
+    setInputs({
+      ...inputs, [name]: value
+    });
+
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos, inputs
+    ]);
+  };
+
+  return (
+    <Fragment>
+      <form onSubmit={handleSubmit}>
+        Test01<input type="text" name="userId" value={userId} onChange={onChange} />
+        Test02<input type="text" name="id" value={id} onChange={onChange} />
+        Test03<input type="text" name="title" value={title} onChange={onChange} />
+        Test04<input type="text" name="completed" value={completed} onChange={onChange} />
+        <button type="submit">submit</button>
+      </form>
+    </Fragment>
+  );
+}
+
+export default App;
