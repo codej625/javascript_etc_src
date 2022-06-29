c1ad7ac093c2603e1a5ad46eb6b3d8d9867b830e
codej625 <codej625@gmail.com>
2022-06-30 02:50:20 +0900

    20220630_update

--- react/react_crud/test/src/App.js
+++ react/react_crud/test/src/App.js
@@ -1,62 +1,60 @@
 import './App.css';
-// import datas from './todos.json';
+import datas from './todos.json';
 import React, { Fragment, useEffect, useState } from 'react';
-import InputTest from './component/InputTest'
-
+//======================================================================
 function App() {
-  const [todos, setTodos] = useState(false);
-
-  // useEffect(() => {
-  //   setTodos(datas);
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log(todos)
-  // }, [todos]);
-
-  // useEffect(() => {
-  //   console.log('userId-> ' + userId)
-  //   console.log('id-> ' + id)
-  //   console.log('title-> ' + title)
-  //   console.log('completed-> ' + completed)
-  // });
-
-  const [inputs, setInputs] = useState({
-    userId: '',
-    id: '',
-    title: '',
-    completed: ''
-  });
-
-  const { userId, id, title, completed } = inputs;
-
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    
-    setInputs({
-      ...inputs, [name]: value
-    });
-
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setTodos([
-      ...todos, inputs
-    ]);
+  //======================================================================
+  const [todos, setTotos] = useState(false);
+  const [update, setUpdate] = useState(false);
+  //======================================================================
+  useEffect(() => {
+    setTotos(datas);
+  }, []);
+  //======================================================================
+  const isUpdate = () => {
+    setUpdate(!update);
+    console.log("update")
   };
-
+  //======================================================================
+  const isDelete = (id) => {
+    if (window.confirm('삭제하시겠습니까?')) {
+      setTotos(todos.filter(del => del.id !== id));
+    };
+  }
+  //======================================================================
   return (
     <Fragment>
-      {/* <form onSubmit={handleSubmit}>
-        Test01<input type="text" name="userId" value={userId} onChange={onChange} />
-        Test02<input type="text" name="id" value={id} onChange={onChange} />
-        Test03<input type="text" name="title" value={title} onChange={onChange} />
-        Test04<input type="text" name="completed" value={completed} onChange={onChange} />
-        <button type="submit">submit</button>
-      </form> */}
-
-      <InputTest />
-    </Fragment>
+      <table>
+        <thead>
+          <tr>
+            {
+              todos && (
+                Object.keys(todos[0]).map(data => (
+                  <th key={data}>{data}</th>
+                ))
+              )
+            }
+            <th colSpan={2}></th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            todos && (
+              todos.map(data => (
+                <tr key={data.id}>
+                  <td>{data.id}</td>
+                  <td>{data.userId}</td>
+                  <td>{data.title}</td>
+                  <td><input type={"checkbox"} defaultChecked={(data.completed) ? true : false} /></td>
+                  <td><input type={"button"} value={"UPDATE"} onClick={isUpdate} /></td>
+                  <td><input type={"button"} value={"DELETE"} onClick={() => isDelete(data.id)} /></td>
+                </tr>
+              ))
+            )
+          }
+        </tbody>
+      </table>
+    </Fragment >
   );
 }
 
