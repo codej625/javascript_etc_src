820d20e406e4833bd015a4bd71c6f7e456efd20b
codej625 <codej625@gmail.com>
2023-04-30 23:09:57 +0900

    20230430 node.js express => cors

--- node.js/express_01/app.js
+++ node.js/express_01/app.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors()); // cors 처리 조건은 x
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Express server on port ${port}`)
-})
+});
